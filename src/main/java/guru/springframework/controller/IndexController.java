d16c97e497401b77653ef6dcd9183732f5ce6508
p0tion <toulskikh@gmail.com>
2018-05-10 14:35:49 +0300

    add dynamic finders for the Category and UnitOfMeasure classes

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.controller;
 
+import guru.springframework.model.Category;
+import guru.springframework.model.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Pinch");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 
