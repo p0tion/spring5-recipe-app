49208129df31c9ac0d0171f4811e7e38893cd9d6
p0tion <toulskikh@gmail.com>
2018-05-10 17:35:31 +0300

    add RecipeService, update index.html

---
+++ src/main/java/guru/springframework/service/RecipeServiceImpl.java
@@ -0,0 +1,26 @@
+package guru.springframework.service;
+
+import guru.springframework.model.Recipe;
+import guru.springframework.repository.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().forEach(recipeSet::add);
+        return recipeSet;
+    }
+
+}
